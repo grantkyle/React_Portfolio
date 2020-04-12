c02c17a522e1f1b0428c517db0ad950cfa0180c2
Grant Kyle <sgrantkyle@gmail.com>
2020-04-11 20:29:37 -0500

    header and footer complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 import './App.css';
+
+import Footer from './components/Footer';
 
 class App extends React.Component {
 
@@ -32,8 +36,23 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-      <Container fluid={true}>
-        <p>H1 From React</p>
+      <Container className="p-0" fluid={true}>
+
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+          <Navbar.Brand>Grant</Navbar.Brand>
+
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/about">About</Link>
+            <Link className="nav-link" to="/contact">Contact</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Footer />
+
       </Container>
       </Router>
   );
