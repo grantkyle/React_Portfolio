47c7d0b5fd087136e2fac5178a2aac34f114efa2
Grant Kyle <sgrantkyle@gmail.com>
2020-04-11 21:53:18 -0500

    hero page addition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
@@ -12,14 +12,14 @@ import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       title: "Grant Kyle",
       headerLinks: [
-        {title: 'Home', path: "/"},
-        {title: 'About', path: "/about"},
-        {title: 'Contact', path: "/contact"},
+        { title: 'Home', path: "/" },
+        { title: 'About', path: "/about" },
+        { title: 'Contact', path: "/contact" },
       ],
       home: {
         title: 'Portfolio',
@@ -28,43 +28,43 @@ class App extends React.Component {
       },
       about: {
         title: 'About Me',
-    },
+      },
       contact: {
         title: 'Contact',
       }
-  }
+    }
 
   }
 
   render() {
     return (
       <Router>
-      <Container className="p-0" fluid={true}>
+        <Container className="p-0" fluid={true}>
 
-        <Navbar className="border-bottom" bg="transparent" expand="lg">
-          <Navbar.Brand>Grant</Navbar.Brand>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Grant</Navbar.Brand>
 
-          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
-          <Navbar.Collapse id="navbar-toggle">
-            <Nav className="ml-auto">
-            <Link className="nav-link" to="/">Home</Link>
-            <Link className="nav-link" to="/about">About</Link>
-            <Link className="nav-link" to="/contact">Contact</Link>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
 
-            
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-        <Route path="/" exact render={() => <AboutPage title={this.state.home.title}/> } />
-        <Route path="/" exact render={() => <ContactPage title={this.state.home.title}/> } />
 
-        <Footer />
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
-      </Container>
+          <Footer />
+
+        </Container>
       </Router>
-  );
-    }
+    );
+  }
 }
 
 export default App;
